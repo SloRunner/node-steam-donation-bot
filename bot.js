607e7c8b08df1e04c141d9420c401f09095a35b4
SloRunner <slo.runner.samp@gmail.com>
2017-10-03 17:13:34 +0300

    Use JavaScript Standard Style (https://standardjs.com)

--- bot.js
+++ bot.js
@@ -1,74 +1,72 @@
-const SteamUser = require("steam-user");
-const SteamTotp = require('steam-totp');
-const fs = require('fs');
-const TradeOfferManager = require('steam-tradeoffer-manager');
-var msg = '';
+const SteamUser = require('steam-user')
+const SteamTotp = require('steam-totp')
+const fs = require('fs')
+const TradeOfferManager = require('steam-tradeoffer-manager')
+var msg = ''
 
 if (fs.existsSync('./config.json')) {
-    var config = require('./config.json');
-}else{
-	console.log('Config file not present, please create one or copy it from config.example.json file');
-	process.exit(1);
+  var config = require('./config.json')
+} else {
+  console.log('Config file not present, please create one or copy it from config.example.json file')
+  process.exit(1)
 }
-const client = new SteamUser();
+const client = new SteamUser()
 const manager = new TradeOfferManager({
-	"steam": client,
-	"domain": "localhost",
-	"language": "en",
-	"pollInterval": 30000
-});
+  'steam': client,
+  'domain': 'localhost',
+  'language': 'en',
+  'pollInterval': 30000
+})
 
 // Login with bot account
 client.logOn({
-	"accountName": config.bot_username,
-	"password": config.bot_password,
-	"twoFactorCode": SteamTotp.getAuthCode(config.bot_totp)
-});
+  'accountName': config.bot_username,
+  'password': config.bot_password,
+  'twoFactorCode': SteamTotp.getAuthCode(config.bot_totp)
+})
 
 // Logged on event is fired
-client.on("loggedOn", function () {
-	console.log("Logged on to steam!");
-	if (config.bot_online == true) {
-		//Set bot online if requested in config
-		client.setPersona(1);
-		console.log("Setting status to online!");
-	};
-});
+client.on('loggedOn', function () {
+  console.log('Logged on to steam!')
+  if (config.bot_online === true) {
+    // Set bot online if requested in config
+    client.setPersona(1)
+    console.log('Setting status to online!')
+  };
+})
 
 // Catch webSession event
-client.on('webSession', function(sessionID, cookies) {
-	//Set cookies to the trade offer manager
-	manager.setCookies(cookies, function(err) {
-		if (err) {
-			console.log(err);
-			process.exit(1); // Exit since we did not get api key
-			return;
-		}
-
-		console.log("Got API key: " + manager.apiKey);
-	});
-});
+client.on('webSession', function (sessionID, cookies) {
+  // Set cookies to the trade offer manager
+  manager.setCookies(cookies, function (err) {
+    if (err) {
+      console.log(err)
+      process.exit(1) // Exit since we did not get api key
+    }
+    console.log('Got API key: ' + manager.apiKey)
+  })
+})
 
 // Catch new offer event
-manager.on('newOffer', function(offer) {
-	var recieved_items = offer.itemsToReceive;
-	var given_items = offer.itemsToGive;
-	// Only accept offer if we are not giving any items
-	if (given_items.length == 0 && recieved_items.length > 0) {
-		recieved_items = recieved_items.map(item => item.name)
-		offer.accept(false, function(){
-			client.getPersonas([offer.partner], function(persona){
-				msg = 'Recieved: ' + recieved_items.join(', ') + ' from ' + persona[offer.partner].player_name + ' [' + offer.partner + ']';
-				console.log(msg);
-				if (config.send_message == true) {
-					client.chatMessage(config.message_to, msg);
-				};
-			});
-		});
-	};
-});
+manager.on('newOffer', function (offer) {
+  var recieveditems = offer.itemsToReceive
+  var givenitems = offer.itemsToGive
+  // Only accept offer if we are not giving any items
+  if (givenitems.length === 0 && recieveditems.length > 0) {
+    recieveditems = recieveditems.map(item => item.name)
+    offer.accept(false, function () {
+      client.getPersonas([offer.partner], function (persona) {
+        msg = 'Recieved: ' + recieveditems.join(', ') + ' from ' + persona[offer.partner].player_name + ' [' + offer.partner + ']'
+        console.log(msg)
+        if (config.send_message === true) {
+          client.chatMessage(config.message_to, msg)
+        };
+      })
+    })
+  };
+})
 
 // Write pollData when we get it
-manager.on('pollData', function(pollData) {
-	fs.writeFile('polldata.json', JSON.stringify(pollData));
-});
+manager.on('pollData', function (pollData) {
+  fs.writeFile('polldata.json', JSON.stringify(pollData))
+})
